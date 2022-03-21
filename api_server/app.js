3470be975fdad6e85e16a85c882d858ddf5a3262
Jingying He <450883724@qq.com>
2022-03-21 22:08:06 +1100

    set router

--- api_server/app.js
+++ api_server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const userRouter = require()
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
