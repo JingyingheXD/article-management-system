3321e245b48770c1b30bc51ae7d1de429d18cd78
Jingying He <450883724@qq.com>
2022-03-21 15:38:14 +1100

    add cors

--- api_server/app.js
+++ api_server/app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
 
 app.listen(3007, () => {
   console.log("api server running at http://127.0.0.1:3007");
