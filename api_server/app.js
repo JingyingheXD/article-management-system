7df593c25e1aafbe19d60151912778acf4136cbc
Jingying He <450883724@qq.com>
2022-03-21 22:27:48 +1100

    configuration

--- api_server/app.js
+++ api_server/app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const userRouter = require()
+const userRouter = require("./router/user");
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
+app.use("/api", userRouter);
 
 app.listen(3007, () => {
   console.log("api server running at http://127.0.0.1:3007");
