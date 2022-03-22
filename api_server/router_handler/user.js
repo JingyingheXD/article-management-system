ab376a79018564c6df969fa4045713d233e9fce2
Jingying He <450883724@qq.com>
2022-03-22 16:33:16 +1100

    check if username exists in db

--- api_server/router_handler/user.js
+++ api_server/router_handler/user.js
@@ -1,3 +1,5 @@
+const db = require("../db/index");
+
 exports.register = (req, res) => {
   const userinfo = req.body;
   if (!userinfo.username || !userinfo.password) {
@@ -6,6 +8,21 @@ exports.register = (req, res) => {
       message: "Username or Password should not be null",
     });
   }
+
+  const sqlStr = `SELECT * FROM ev_users WHERE username=?`;
+  db.query(sqlStr, userinfo.username, (err, results) => {
+    if (err) {
+      return res.send({ status: 1, message: err.message });
+    }
+
+    if (results.length > 0) {
+      return res.send({
+        status: 1,
+        message: "This usernmae exists, please change another one.",
+      });
+    }
+    // TODO: username can be used
+  });
 
   res.send("register OK");
 };
