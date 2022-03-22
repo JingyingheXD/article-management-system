57e05ed5cb97e8664adfae01b06ed2a55cce381f
Jingying He <450883724@qq.com>
2022-03-22 23:53:44 +1100

    reset db

--- api_server/db/index.js
+++ api_server/db/index.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const db = mysql.createPool({
   host: "127.0.0.1",
   user: "root",
-  password: "admin123",
+  password: "Admin123",
   database: "my_db_01",
 });
 
