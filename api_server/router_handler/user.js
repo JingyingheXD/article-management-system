d0226d1a190b04480c7885b32d56e9965c93c457
Jingying He <450883724@qq.com>
2022-03-22 16:19:31 +1100

    check if input data validate

--- api_server/router_handler/user.js
+++ api_server/router_handler/user.js
@@ -1,4 +1,12 @@
 exports.register = (req, res) => {
+  const userinfo = req.body;
+  if (!userinfo.username || !userinfo.password) {
+    return res.send({
+      status: 1,
+      message: "Username or Password should not be null",
+    });
+  }
+
   res.send("register OK");
 };
 
