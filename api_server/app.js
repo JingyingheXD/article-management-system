b30163e04fcd01abcf070934c7ee3feeaf4e0b2b
Jingying He <450883724@qq.com>
2022-03-25 20:44:44 +1100

    update request object format

--- api_server/app.js
+++ api_server/app.js
@@ -8,6 +8,7 @@ const config = require("./config");
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.use((req, res, next) => {
   res.cc = (err, status = 1) => {
