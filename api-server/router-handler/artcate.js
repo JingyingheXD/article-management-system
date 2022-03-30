bfa3ee92806e8a1a5956b9fd3f3e364cdc77b52f
Jingying He <450883724@qq.com>
2022-03-30 22:03:06 +1100

    check when add article cate

--- api-server/router-handler/artcate.js
+++ api-server/router-handler/artcate.js
@@ -14,5 +14,23 @@ exports.getArtCates = (req, res) => {
 };
 
 exports.addArticleCates = (req, res) => {
-  res.send("OK");
+  const sql = `SELECT * FROM ev_article_cate WHERE name=? or alias=?`;
+  db_current.query(sql, [req.body.name, req.body.alias], (err, results) => {
+    if (err) return res.cc(err);
+
+    if (results.length === 2)
+      return res.cc("Name and alias exist, please change new name and alias.");
+    if (
+      results.length === 1 &&
+      results[0].name === req.body.name &&
+      results[0].alias === req.body.alias
+    )
+      return res.cc("Name and alias exist, please change new name and alias.");
+    if (results.length === 1 && results[0].name === req.body.name)
+      return res.cc("Name exists, please change a new name.");
+    if (results.length === 1 && results[0].alias === req.body.alias)
+      return res.cc("Alias exists, please change a new alias.");
+
+    // TODO: can add new article cate
+  });
 };
