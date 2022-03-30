2a5e33635de80886be2aeda3fd43fb4d97f533d8
Jingying He <450883724@qq.com>
2022-03-30 15:42:06 +1100

    validate update user information

--- api-server/router/userinfo.js
+++ api-server/router/userinfo.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const userinfoHandler = require("../router-handler/userinfo");
+const expressJoi = require("@escook/express-joi");
+const { update_userinfo_schema } = require("../schema/user");
 
-router.get("/userinfo", userinfoHandler.getUserInfo);
-router.post("/userinfo", userinfoHandler.updateUserInfo);
+router.get(
+  "/userinfo",
+  expressJoi(update_userinfo_schema),
+  userinfoHandler.getUserInfo
+);
+router.post(
+  "/userinfo",
+  expressJoi(update_userinfo_schema),
+  userinfoHandler.updateUserInfo
+);
 
 module.exports = router;
