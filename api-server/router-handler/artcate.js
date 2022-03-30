4f45bf9753fe7c1d486ecdd3e812a7e775124459
Jingying He <450883724@qq.com>
2022-03-30 21:24:34 +1100

    get article cate data

--- api-server/router-handler/artcate.js
+++ api-server/router-handler/artcate.js
@@ -1,3 +1,14 @@
+const db = require("../db/index");
+const db_current = process.env.NODE_ENV === "test" ? db.db_test : db.db;
+
 exports.getArtCates = (req, res) => {
-  res.send("OK");
+  const sql = `SELECT * FROM ev_article_cate WHERE is_delete=0 ORDER BY id ASC`;
+  db_current.query(sql, (err, results) => {
+    if (err) return res.cc(err);
+    res.send({
+      status: 0,
+      message: "Get article categories data successfully.",
+      data: results,
+    });
+  });
 };
