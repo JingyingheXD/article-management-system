a0476eceebd7a8bd315c75e430895476a50ac6e4
Jingying He <450883724@qq.com>
2022-04-02 21:16:23 +1100

    install and import multer

--- api-server/router-handler/article.js
+++ api-server/router-handler/article.js
@@ -2,5 +2,9 @@ const db = require("../db/index");
 const db_current = process.env.NODE_ENV === "test" ? db.db_test : db.db;
 
 exports.addArticle = (req, res) => {
+  console.log(req.body);
+  console.log("-----------------------");
+  console.log(req.file);
+
   res.send("OK");
 };
--- api-server/router/article.js
+++ api-server/router/article.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const articleHandler = require("../router-handler/article");
+const multer = require("multer");
+const path = require("path");
 
-router.post("/add", articleHandler.addArticle);
+const upload = multer({ dest: path.join(__dirname, "../uploads") });
+
+router.post("/add", upload.single("cover_img"), articleHandler.addArticle);
 
 module.exports = router;
