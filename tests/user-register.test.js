9b2bd2a432c8946b5c16490f9cca30d37aae42e1
Jingying He <450883724@qq.com>
2022-04-03 13:31:36 +1000

    API testing - register

--- tests/user-register.test.js
+++ tests/user-register.test.js
@@ -4,41 +4,78 @@ const db = require("../api-server/db/index");
 const db_current = db.db_test;
 
 describe("POST /api/register", () => {
-  describe("input with invalid username and password", () => {
-    beforeEach((done) => {
-      db_current.query("DELETE FROM ev_users", (err, results) => {
-        if (err) {
-          throw done(err);
-        } else {
-          done();
-        }
-      });
+  beforeEach((done) => {
+    db_current.query(`DELETE FROM ev_users`, (err, results) => {
+      if (err) {
+        throw done(err);
+      } else {
+        done();
+      }
     });
-
-    test("should respond with status 1 when not pass a username", (done) => {
-      request(app)
-        .post("/api/register")
-        .send({
-          password: "password",
-        })
-        .then((response) => {
-          expect(response.body.status).toBe(1);
-
-          const sql = `SELECT COUNT(*) AS user_count FROM ev_users`;
-          db_current.query(sql, (err, results) => {
-            if (err) throw done(err);
-            const rowsNumber = results[0].user_count;
-            expect(rowsNumber).toBe(0);
-            done();
-          });
-        });
-    });
-
-    // test("should respond with status 1 when username including symbol", async () => {});
-    // test("should respond with status 1 when password length less than 6", async () => {});
   });
-  // describe("input with valid username and password", () => {
-  //   test("should respond with status 1 and message when the username already exists in the database", async () => {});
-  //   test("should respond with status 0 and message when the user register successfully", async () => {});
-  // });
+
+  const respond = (done, userCase, ExpectedStuatus, ExpectedRowsNumber) => {
+    const res = request(app)
+      .post("/api/register")
+      .send(userCase)
+      .then((response) => {
+        expect(response.body.status).toBe(ExpectedStuatus);
+
+        const sql = `SELECT COUNT(*) AS user_count FROM ev_users`;
+        db_current.query(sql, (err, results) => {
+          if (err) throw done(err);
+          const rowsNumber = results[0].user_count;
+          expect(rowsNumber).toBe(ExpectedRowsNumber);
+          done();
+        });
+      });
+  };
+
+  describe("input with invalid username and password", () => {
+    test("should respond with status 1 when not pass a username", (done) => {
+      const userCase1 = {
+        password: "password",
+      };
+      respond(done, userCase1, 1, 0);
+    });
+
+    test("should respond with status 1 when username including symbols", (done) => {
+      const userCase2 = {
+        username: "username!@#",
+        password: "password",
+      };
+      respond(done, userCase2, 1, 0);
+    });
+    test("should respond with status 1 when password length less than 6", (done) => {
+      const userCase3 = {
+        username: "username",
+        password: "pwd",
+      };
+      respond(done, userCase3, 1, 0);
+    });
+  });
+
+  describe("input with valid username and password", () => {
+    test("should respond with status 1 and message when the username already exists in the database", (done) => {
+      db_current.query(
+        `INSERT INTO ev_users (username, password) VALUES ('Jane', 'asdfgh')`,
+        (err, results) => {
+          if (err) throw done(err);
+          const userCase4 = {
+            username: "Jane",
+            password: "123456",
+          };
+          respond(done, userCase4, 1, 1);
+        }
+      );
+    });
+
+    test("should respond with status 0 and message when the user register successfully", (done) => {
+      const userCase5 = {
+        username: "Jenny",
+        password: "098765",
+      };
+      respond(done, userCase5, 0, 1);
+    });
+  });
 });
