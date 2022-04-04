fec7b5289a49af55ed553bf8442d8398a9250c7f
Jingying He <450883724@qq.com>
2022-04-04 23:24:25 +1000

    update login handler by using model function

--- api-server/controllers/user.js
+++ api-server/controllers/user.js
@@ -1,11 +1,10 @@
 const db = require("../db/index");
 const db_current = process.env.NODE_ENV === "test" ? db.db_test : db.db;
 
+const userModel = require("../models/user");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const config = require("../config");
-
-const userModel = require("../models/user");
 
 exports.register = (req, res) => {
   const userinfo = req.body;
@@ -35,20 +34,19 @@ exports.register = (req, res) => {
 
 exports.login = (req, res) => {
   const userinfo = req.body;
-  const sql = `SELECT * FROM ev_users WHERE username=?`;
 
-  db_current.query(sql, userinfo.username, (err, results) => {
-    if (err) return res.cc(err);
-    if (results.length !== 1) return res.cc("Login failed.");
+  userModel.read(userinfo.username, (readErr, readResults) => {
+    if (readErr) return res.cc(readErr);
+    if (readResults.length !== 1) return res.cc("Login failed.");
 
     const compareResult = bcrypt.compareSync(
       userinfo.password,
-      results[0].password
+      readResults[0].password
     );
     if (!compareResult) return res.cc("Wrong password.");
 
     // remove private information when generating Token
-    const user = { ...results[0], password: "", user_pic: "" };
+    const user = { ...readResults[0], password: "", user_pic: "" };
     const tokenStr = jwt.sign(user, config.jwtSecretKey, {
       expiresIn: config.expireIn,
     });
